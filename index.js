7f17703ed16fd83b53ae2d464c11b4cdf9f86087
Mariano Belgrano <mbelgrano@gmail.com>
2025-11-12 12:15:52 -0300

    Se agregó npm run dev con el flag --watch

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: ["http://localhost:5173",],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// 4. MIDDLEWARE PARA MANEJO DE ERRORES
+
+// Middleware 404 - Debe ir después de todas las rutas
+app.use((req, res, next) => {
+    res.status(404).send('Recurso no encontrado');
+});
+
+// Middleware de manejo de errores general
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    if (res.headersSent) return next(err);
+    res.status(500).send('¡Algo salió mal!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Deployed on url https://localhost:${PORT}`);
+});
